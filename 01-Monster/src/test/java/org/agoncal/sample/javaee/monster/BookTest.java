359252a07fef3144c2f4364e91fd9fc0cb303f50
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-06-24 16:52:16 +0200

    Update BookTest.java

--- 01-Monster/src/test/java/org/agoncal/sample/javaee/monster/BookTest.java
+++ 01-Monster/src/test/java/org/agoncal/sample/javaee/monster/BookTest.java
@@ -1,20 +1,12 @@
 package org.agoncal.sample.javaee.monster;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.ejb.embeddable.EJBContainer;
-import javax.naming.Context;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
-import java.io.File;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Antonio Goncalves
@@ -27,7 +19,7 @@ public class BookTest {
     // =             Attributes             =
     // ======================================
 
-    public static final String creditCardXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><MonsterXML><isbn>1234-5678-9010</isbn><nbOfPage>135</nbOfPage><illustrations>true</illustrations><contentLanguage>EN</contentLanguage><title>Dummy JAXB</title><price>10.5</price><description>JAXB Marshalling</description></MonsterXML>";
+    public static final String creditCardXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><book><isbn>1234-5678-9010</isbn><nbOfPage>135</nbOfPage><illustrations>true</illustrations><contentLanguage>EN</contentLanguage><title>Dummy JAXB</title><price>10.5</price><description>JAXB Marshalling</description></book>";
 
     // ======================================
     // =              Unit tests            =
