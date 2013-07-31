ea880a0572e62be119e1a2c47b9e741dd072accf
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-07-31 15:58:21 +0200

    Create Book.java

---
+++ 02-JavaOne2013/src/main/java/org/agoncal/application/javaone2013/model/Book.java
@@ -0,0 +1,134 @@
+package org.agoncal.application.javaone2013.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b")
+})
+public class Book {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    private String isbn;
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @Size(min = 10, max = 2000)
+    private String description;
+    private Float price;
+    private Integer nbOfPage;
+    private Date publicationDate;
+    private String publisher;
+    private Language language;
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+
+    public Book() {
+    }
+
+    public Book(String title, String description, Float price, String isbn, Integer nbOfPage) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.isbn = isbn;
+        this.nbOfPage = nbOfPage;
+    }
+
+    // ======================================
+    // =          Getters & Setters         =
+    // ======================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getNbOfPage() {
+        return nbOfPage;
+    }
+
+    public void setNbOfPage(Integer nbOfPage) {
+        this.nbOfPage = nbOfPage;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+}
