c9fb464160c80ac182a38bb57aa9f47ee8cb62e5
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-10-19 21:01:59 +0200

    Adding SQL delete statement

--- 03-TierArchitecture/01-Horizontal/src/main/resources/insert.sql
+++ 03-TierArchitecture/01-Horizontal/src/main/resources/insert.sql
@@ -1,3 +1,39 @@
+DELETE FROM book_author WHERE book_id=1000 AND authors_id=1000
+DELETE FROM book_author WHERE book_id=1001 AND authors_id=1001
+DELETE FROM book_author WHERE book_id=1002 AND authors_id=1002
+DELETE FROM book_author WHERE book_id=1003 AND authors_id=1003
+DELETE FROM book_author WHERE book_id=1003 AND authors_id=1004
+DELETE FROM book_author WHERE book_id=1004 AND authors_id=1005
+DELETE FROM book_author WHERE book_id=1004 AND authors_id=1006
+DELETE FROM book_author WHERE book_id=1006 AND authors_id=1007
+DELETE FROM book_author WHERE book_id=1007 AND authors_id=1008
+DELETE FROM book_author WHERE book_id=1008 AND authors_id=1009
+DELETE FROM book_author WHERE book_id=1011 AND authors_id=1011
+DELETE FROM book_author WHERE book_id=1012 AND authors_id=1012
+DELETE FROM book_author WHERE book_id=1012 AND authors_id=1013
+
+DELETE FROM book WHERE id=1000
+DELETE FROM book WHERE id=1001
+DELETE FROM book WHERE id=1002
+DELETE FROM book WHERE id=1003
+DELETE FROM book WHERE id=1004
+
+DELETE FROM author WHERE id=1000
+DELETE FROM author WHERE id=1001
+DELETE FROM author WHERE id=1002
+DELETE FROM author WHERE id=1003
+DELETE FROM author WHERE id=1004
+DELETE FROM author WHERE id=1005
+DELETE FROM author WHERE id=1006
+DELETE FROM author WHERE id=1007
+DELETE FROM author WHERE id=1008
+DELETE FROM author WHERE id=1009
+DELETE FROM author WHERE id=1010
+DELETE FROM author WHERE id=1011
+DELETE FROM author WHERE id=1012
+DELETE FROM author WHERE id=1013
+
+
 INSERT INTO book(id, version, isbn, title, language, description, price, nbOfPages, publisher, publicationDate, imageURL, pageURL) VALUES (1000, 0, '143024626X', 'Beginning Java EE 7',                                                              0, 'Java Enterprise Edition (Java EE) continues to be one of the leading Java technologies and platforms. Beginning Java EE 7 is the first tutorial book on Java EE 7. Step by step and easy to follow, this book describes many of the Java EE 7 specifications...', 49.99, 608, 'APress',    '2013-06-26', 'http://ecx.images-amazon.com/images/I/518rDqWwjhL._SL160_.jpg', 'http://www.amazon.com/Beginning-Java-EE-Antonio-Goncalves/dp/143024626X%3FSubscriptionId%3DAKIAIYNLC7WME6YSY66A%26tag%3Daztag-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D143024626X')
 INSERT INTO book(id, version, isbn, title, language, description, price, nbOfPages, publisher, publicationDate, imageURL, pageURL) VALUES (1001, 0, '1449370179', 'Java EE 7 Essentials',                                                             0, 'Get up to speed on the principal technologies in the Java Platform, Enterprise Edition 7, and learn how the latest version embraces HTML5, focuses on higher productivity, and provides functionality to meet enterprise demands. Written by Arun Gupta, a k...', 39.99, 362, 'O''Reilly', '2013-09-03', 'http://ecx.images-amazon.com/images/I/51XmtrCbZ5L._SL160_.jpg', 'http://www.amazon.com/Java-EE-Essentials-Arun-Gupta/dp/1449370179%3FSubscriptionId%3DAKIAIYNLC7WME6YSY66A%26tag%3Daztag-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1449370179')
 INSERT INTO book(id, version, isbn, title, language, description, price, nbOfPages, publisher, publicationDate, imageURL, pageURL) VALUES (1002, 0, '1430244259', 'Java EE 7 Recipes: A Problem-Solution Approach',                                   0, 'Java EE 7 Recipes takes an example-based approach in showing how to program Enterprise Java applications in many different scenarios. Be it a small-business web application, or an enterprise database application, Java EE 7 Recipes provides effective an...', 49.99, 748, 'APress',    '2013-05-22', 'http://ecx.images-amazon.com/images/I/51MKty2UO2L._SL160_.jpg', 'http://www.amazon.com/Java-EE-Recipes-Problem-Solution-Approach/dp/1430244259%3FSubscriptionId%3DAKIAIYNLC7WME6YSY66A%26tag%3Daztag-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1430244259')
@@ -20,15 +56,15 @@ INSERT INTO author(id, version, firstname, surname, bio)          VALUES (1012, 
 INSERT INTO author(id, version, firstname, surname, bio)          VALUES (1013, 0, 'Fred', 'Rowe',          'Fred is the WebSphere Architect for a number of Application Server components including those responsible for JEE managed concurrency, JCA (Java Connector Architecture), and database connectivity and connection management. Fred has worked at IBM for 10...')
 
 INSERT INTO book_author(book_id, authors_id) VALUES (1000, 1000)
-INSERT INTO book_author(book_id, authors_id) VALUES (1001, 1001)
-INSERT INTO book_author(book_id, authors_id) VALUES (1002, 1002)
-INSERT INTO book_author(book_id, authors_id) VALUES (1003, 1003)
-INSERT INTO book_author(book_id, authors_id) VALUES (1003, 1004)
-INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1005)
-INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1006)
-INSERT INTO book_author(book_id, authors_id) VALUES (1006, 1007)
-INSERT INTO book_author(book_id, authors_id) VALUES (1007, 1008)
-INSERT INTO book_author(book_id, authors_id) VALUES (1008, 1009)
-INSERT INTO book_author(book_id, authors_id) VALUES (1011, 1011)
-INSERT INTO book_author(book_id, authors_id) VALUES (1012, 1012)
-INSERT INTO book_author(book_id, authors_id) VALUES (1012, 1013)
+INSERT INTO book_author(book_id, authors_id) VALUES (1000, 1001)
+INSERT INTO book_author(book_id, authors_id) VALUES (1001, 1002)
+INSERT INTO book_author(book_id, authors_id) VALUES (1001, 1003)
+INSERT INTO book_author(book_id, authors_id) VALUES (1001, 1004)
+INSERT INTO book_author(book_id, authors_id) VALUES (1001, 1005)
+INSERT INTO book_author(book_id, authors_id) VALUES (1002, 1006)
+INSERT INTO book_author(book_id, authors_id) VALUES (1003, 1007)
+INSERT INTO book_author(book_id, authors_id) VALUES (1003, 1008)
+INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1009)
+INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1011)
+INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1012)
+INSERT INTO book_author(book_id, authors_id) VALUES (1004, 1013)
